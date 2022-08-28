2fc021c389d46403d5e7826b47d79744e2b14a65
fugita <81188207+fufugita@users.noreply.github.com>
2022-08-28 02:47:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,18 @@ import time
 
 def get_arguments():
     parser = argparse.ArgumentParser(description="Python Port Scanner - A python script that scans for open ports on a host")
-    parser.add_argument("-t", "--target", action="store", help="This option requires you to provide a target ip address to scan.")
+    parser.add_argument("-t", "--target", action="store", help="This option requires you to provide a target ip address to scan, if none is given it will scan the localhost instead.")
     parser.add_argument("-p", "--ports", action="store", help="This option requires you to provide a single port or port range to scan (1-65535).")
-    parser.add_argument("-l", "--localhost", action="store", help="This option scans the localhost.")
     parser.add_argument("-w", "--web", action="store", help="This option scans for any existing web server running on the port 80\HTTP or 443\HTTPS on a host.")
     parser.add_argument("-o", "--options", action="store", help="One of the three options must be selected: 1 - Scans the 10 most common ports, 2 - Scans for only priviliged ports (1-1023), 3 -.")
     args = parser.parse_args()
 
     if not args.target:
-        print("Please provide a target ip address, scanning localhost instead...")
+        print("Scanning localhost...")
         quit()
 
-    if not args.ports and not args.localhost and not args.options and not args.web:
+    if not args.ports and not args.options and not args.web:
         args.ports = "1-65535"
-    
-    if args.localhost:
-        print("Scanning localhost...")
-        args.target = '127.0.0.1'
 
     if args.web:
         print("Scanning ports 80 and 443...")
@@ -33,12 +28,12 @@ def get_arguments():
     if args.options:
         if args.ports:
             print("Scanning given port(s)...")
-        if args.mode == "1":
+        if args.options == "1":
             args.ports = [20, 21, 22, 23, 25, 53, 80, 110, 119, 443]
-        elif args.mode == "2":
-            args.ports = "1-1023"
-        elif args.mode == "3":
+        elif args.options == "2":
             print("idk") 
+        elif args.options == "3":
+            print("also dk") 
         else:
             print("Invalid option, quitting...")
             quit()
@@ -51,7 +46,7 @@ class PortScanner:
 
         self.target = target
         self.ports = ports
-        self.target = options
+        self.options = options
     
     # Função que verifica se a porta está aberta com o 'socket'
     def scan_ports(self, port):
@@ -62,6 +57,7 @@ class PortScanner:
 
             s.connect((self.target, port))
             port = "{0}".format(port)
+
             try:
                 banner = s.recv(1024).decode()
                 print("Port {0} is open with banner {1}.".format(port, banner))
@@ -69,7 +65,6 @@ class PortScanner:
                 print("Port {0} is open.".format(port))
            
             s.close()
-
         except:
             pass
 
@@ -83,7 +78,6 @@ class PortScanner:
 
         start_time = time.time()
         for port in self.ports:
-            self.scan_ports(port)
             thread = threading.Thread(target=self.scan_ports(port), args=[port])
             thread.start()
         end_time = time.time()
